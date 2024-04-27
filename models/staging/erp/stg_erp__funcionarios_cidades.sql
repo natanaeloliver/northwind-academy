a92156522d6db4ebd1b69a37e10f251d5f0ddaa5
Natanael de Oliveira da Rosa <natanael.rosa@vs.unimed.com.br>
2024-04-27 18:48:27 +0000

    Criando as dim_cliente e dim_transportadora.

--- models/staging/erp/stg_erp__funcionarios_cidades.sql
+++
@@ -1,9 +0,0 @@
-with
-    fonte_funcionarios_cidades as (
-        select
-            cast(EMPLOYEEID as int) as fk_funcionario
-            , cast(TERRITORYID as int) as fk_cidade
-        from {{ source('erp', 'employeeterritory') }}
-    )
-select *
-from fonte_funcionarios_cidades
--- models/staging/erp/stg_erp__regioes.sql
+++
@@ -1,9 +0,0 @@
-with
-    fonte_regioes as (
-        select     
-            cast(ID as int) as pk_regiao
-            , cast(REGIONDESCRIPTION as varchar) as nm_regiao
-        from {{ source('erp', 'region') }}
-    )
-select *
-from fonte_regioes
